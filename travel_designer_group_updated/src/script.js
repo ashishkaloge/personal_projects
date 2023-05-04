65ea8d230293db06da2a787dbad24dafe6283fb3
Ashish Kaloge <110806467+ashishkaloge@users.noreply.github.com>
2023-05-05 00:34:37 +0530

    Add files via upload

---
+++ travel_designer_group_updated/src/script.js
@@ -0,0 +1,63 @@
+// varaible declaration
+const year = new Date().getFullYear();
+const slide = document.getElementById("slide");
+const nodeList = document.querySelectorAll("span");
+
+// function declaration
+function leftremoveElement(leftvalue) {
+    for (let i = 0; i < 2; i++) {
+        if (leftvalue <= year - 27) {
+            break;
+        }
+        slide.lastElementChild.remove();
+    }
+}
+function leftaddElement(leftvalue) {
+    for (var i = nodeList.length - 1; i < nodeList.length + 1; i++) {
+        if (leftvalue <= year - 27) 
+        {
+            break;
+        }
+        leftvalue--;
+        let spanitem = document.createElement("span");
+        let text = document.createTextNode(leftvalue);
+        spanitem.appendChild(text);
+        spanitem.className="px-2 col-4 col-sm-2 col-md-1";
+     
+        slide.insertBefore(spanitem, slide.firstElementChild);
+    }
+}
+function rightremoveElement(rightvalue) {
+    for (var i = 0; i < 2; i++) {
+        if (rightvalue >= year) {
+            break;
+        }
+        slide.firstElementChild.remove();
+    }
+}
+
+function rightaddElement(rightvalue) {
+    for (var i = nodeList.length - 1; i < nodeList.length + 1; i++) {
+        if (rightvalue >= year) {
+            break;
+        }
+        rightvalue++;
+        let spanitem = document.createElement("span");
+        let text = document.createTextNode(rightvalue);
+        spanitem.appendChild(text);
+        spanitem.className="px-2 col-4 col-sm-2 col-md-1";
+        slide.appendChild(spanitem);
+    }
+}
+
+document.getElementById("left").onclick = function () {
+    let leftvalue = slide.firstElementChild.innerText;
+    leftremoveElement(leftvalue);
+    leftaddElement(leftvalue);
+}
+
+document.getElementById("right").onclick = function () {
+    let rightvalue = slide.lastElementChild.innerText;
+    rightremoveElement(rightvalue);
+    rightaddElement(rightvalue);
+}
